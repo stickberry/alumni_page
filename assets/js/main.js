fca25587a65ce33fba68ee5c65baa1e007597cc4
Elaine Mary Rose <iit2016130@iiita.ac.in>
2017-07-31 09:00:59 +0530

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,164 @@
+/*
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+/** 
+  * Template Name: Varsity
+  * Version: 1.0  
+  * Template Scripts
+  * Author: MarkUps
+  * Author URI: http://www.markups.io/
+
+  Custom JS
+  
+
+  1. SEARCH FORM
+  2. ABOUT US VIDEO
+  2. TOP SLIDER
+  3. ABOUT US (SLICK SLIDER) 
+  4. LATEST COURSE SLIDER (SLICK SLIDER) 
+  5. TESTIMONIAL SLIDER (SLICK SLIDER)
+  6. COUNTER
+  7. RELATED ITEM SLIDER (SLICK SLIDER)
+  8. MIXIT FILTER (FOR GALLERY)
+  9. FANCYBOX (FOR PORTFOLIO POPUP VIEW)  
+  11. HOVER DROPDOWN MENU
+  12. SCROLL TOP BUTTON  
+
+  
+**/
+
+jQuery(function($){
+
+    // WHEN CLICK PLAY BUTTON 
+    jQuery('#mu-abtus-video').on('click', function(event) {
+      event.preventDefault();
+      $('body').append("<div id='about-video-popup'><span id='mu-video-close' class='fa fa-close'></span><iframe id='mutube-video' name='mutube-video' frameborder='0' allowfullscreen></iframe></div>");        
+      $("#mutube-video").attr("src", $(this).attr("href"));
+    });         
+    // WHEN CLICK CLOSE BUTTON
+    $(document).on('click','#mu-video-close', function(event) {     
+      $(this).parent("div").fadeOut(1000);
+    });
+    // WHEN CLICK OVERLAY BACKGROUND
+    $(document).on('click','#about-video-popup', function(event) {
+      $(this).remove();
+    });
+
+    
+});
+
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 980px)',
+		narrower: '(max-width: 840px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on narrower.
+			skel.on('+narrower -narrower', function() {
+				$.prioritize(
+					'.important\\28 narrower\\29',
+					skel.breakpoint('narrower').active
+				);
+			});
+
+		// Scrolly links.
+			$('.scrolly').scrolly({
+				speed: 1000,
+				offset: -10
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				expandMode: (skel.vars.mobile ? 'click' : 'hover')
+			});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Button.
+				$(
+					'<div id="navButton">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navButton, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+		// Header.
+		// If the header is using "alt" styling and #banner is present, use scrollwatch
+		// to revert it back to normal styling once the user scrolls past the banner.
+		// Note: This is disabled on mobile devices.
+			if (!skel.vars.mobile
+			&&	$header.hasClass('alt')
+			&&	$banner.length > 0) {
+
+				$window.on('load', function() {
+
+					$banner.scrollwatch({
+						delay:		0,
+						range:		1,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});
+
+			}
+
+	});
+
+})(jQuery);
